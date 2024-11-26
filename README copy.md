4bc5cb3f35a61c56db34d361770c46c77438b0c9
unknown <pboyer.306@gmail.com>
2024-11-26 15:48:51 -0700

    moving files

---
+++ README copy.md
@@ -0,0 +1,41 @@
+# Module 8 Challenge Vehical Builder
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+## Description
+
+This project prompts the user to either create a new vehicle or select an existing vehicle. After going through the creation or selection process, the user can perform certain actions with the selected vehicle.
+https://www.loom.com/share/2be841cbdeed4bf2970040b7000f4821?sid=89f6b26e-cd42-4c7d-ba6d-cbc78baff3d9
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [How to Contribute](#how-to-contribute)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+Clone Repo then NPM Install
+
+## Usage
+
+npm start to run this application.
+This application is a vehicle builder, used to create vehicles in your terminal.
+
+## How to Contribute
+
+Anyone can contribute, please submit a PR
+
+## License
+
+MIT
+
+## Tests
+
+none
+
+## Questions
+
+- GitHub Username: [Zyndrr](https://github.com/Zyndrr)
+- Email: [erin.d.mckanna@gmail.com](mailto:erin.d.mckanna@gmail.com)
---
+++ src/classes/Truck.ts
@@ -0,0 +1,86 @@
+import Vehicle from "./Vehicle.js";
+import Motorbike from "./Motorbike.js";
+import Car from "./Car.js";
+import Wheel from "./Wheel.js";
+import AbleToTow from "../interfaces/AbleToTow.js";
+class Truck extends Vehicle implements AbleToTow {
+  
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCapacity: number;
+
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ) {
+    super();
+
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+
+    if (wheels.length !== 4) {
+      this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+    } else {
+      this.wheels = wheels;
+    }
+    this.towingCapacity = towingCapacity;
+  }
+
+  tow(vehicle: Truck | Motorbike | Car): void {
+    if (vehicle.weight <= this.towingCapacity) {
+      console.log(
+        `${this.make} ${this.model} is towing ${vehicle.make} ${vehicle.model}`
+      );
+    } else {
+      console.log(
+        `${this.make} ${this.model} cannot tow ${vehicle.make} ${vehicle.model} because it is too heavy`
+      );
+    }
+  }
+
+  override printDetails(): void {
+    super.printDetails();
+
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
+    console.log(`Towing Capacity: ${this.towingCapacity} lbs`);
+
+    console.log(
+      `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 3: ${this.wheels[2].getDiameter} inch with a ${this.wheels[2].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 4: ${this.wheels[3].getDiameter} inch with a ${this.wheels[3].getTireBrand} tire`
+    );
+  }
+}
+
+export default Truck;
